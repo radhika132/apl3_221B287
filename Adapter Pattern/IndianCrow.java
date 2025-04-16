341565d258e69a449ab7e84f60abf8e6b7ca125a
221B287 <221B287@juet.co.in>
2025-04-16 15:33:13 +0530

    Adapter Pattern

---
+++ Adapter Pattern/IndianCrow.java
@@ -0,0 +1,14 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("cawing");
+    }
+    
+    public void fly(){
+        System.out.println("Flying high over mountain");
+    }
+    
+    public void eat(){
+        System.out.println("eating pearl");
+    }
+}
+
